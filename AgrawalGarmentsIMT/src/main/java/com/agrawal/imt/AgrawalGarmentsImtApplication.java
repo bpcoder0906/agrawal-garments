a4e71d140dafd3d31bacce727f1b8047a4700a39
bishwadeep <bishwadeep.roy1306@gmail.com>
2017-02-05 11:12:07 +0530

    Initial setup

---
+++ AgrawalGarmentsIMT/src/main/java/com/agrawal/imt/AgrawalGarmentsImtApplication.java
@@ -0,0 +1,12 @@
+package com.agrawal.imt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrawalGarmentsImtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrawalGarmentsImtApplication.class, args);
+	}
+}
